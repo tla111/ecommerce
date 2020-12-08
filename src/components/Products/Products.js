cf16e30ebf061bb73306a79e7d13ac33be5642d4
Tim La <email@example.com>
2020-12-08 08:56:39 -0500

    imported commerce into project

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import {Grid} from '@material-ui/core'
+import React from 'react';
+import { Grid } from '@material-ui/core';
 
-import Product from './Product/Product'
-import useStyles from './styles'
+import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5' },
-  {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10'}
-]
+  {
+    id: 1,
+    name: 'Blue Skies',
+    description: 'Nike Foamposite',
+    price: '$50',
+    image:
+      'https://images.solecollector.com/complex/image/upload/sj5bid0df7z7y25i9i5y.jpg',
+  },
+  {
+    id: 2,
+    name: 'Big Pony Fleece',
+    description: 'Ralph Lauren',
+    price: '$60',
+    image:
+      'https://www.rlmedia.io/is/image/PoloGSI/s7-1399761_lifestyle?$rl_df_pdp_5_7_lif$',
+  },
+];
 
 const Products = () => {
   const classes = useStyles();
 
   return (
     <main className={classes.content}>
-      <div className={classes.toolbar}/>
+      <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
@@ -24,6 +38,6 @@ const Products = () => {
       </Grid>
     </main>
   );
-}
+};
 
-export default Products
+export default Products;
